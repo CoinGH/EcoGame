13d667a0e5a8bb9cac1c95f6726b57c94a58ebff
Андрій Трайтака <traitacka@gmail.com>
2026-03-29 14:06:52 +0300

    v.5.8

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -2,22 +2,22 @@ extends CanvasLayer
 
 @onready var grid: GridContainer = $GridContainer 
 
-var tex_empty = preload("res://resources/items/ph_2.png")
+var tex_empty = preload("res://resources/items/empty_slot.png")
 
 const TEXTURE_MAP = {
 	"plastic": preload("res://resources/items/ph_6777.png"),
 	"metal": preload("res://resources/items/ph_67778.png"),
 	"glass": preload("res://resources/items/ph_677.png"),
 	"paper": preload("res://resources/items/ph_677787.png"),
-	"fruit_salad": preload("res://resources/items/ph_1.png"),
-	"handcrafted_game_console": preload("res://resources/items/ph_1.png"),
-	"soup": preload("res://resources/items/ph_1.png"),
-	"handcrafted_accumulator": preload("res://resources/items/ph_1.png"),
-	"electronic": preload("res://resources/items/ph_1.png"),
-	"salt": preload("res://resources/items/ph_1.png"),
-	"fruits": preload("res://resources/items/ph_1.png"),
-	"sugar": preload("res://resources/items/ph_1.png"),
-	"vegetables": preload("res://resources/items/ph_1.png")
+	"fruit_salad": preload("res://resources/items/fs_slot.png"),
+	"handcrafted_game_console": preload("res://resources/items/hgc_slot.png"),
+	"soup": preload("res://resources/items/s_slot.png"),
+	"handcrafted_accumulator": preload("res://resources/items/ha_slot.png"),
+	"electronic": preload("res://resources/items/el_slot.png"),
+	"salt": preload("res://resources/items/salt_slot.png"),
+	"fruits": preload("res://resources/items/fruits_slot.png"),
+	"sugar": preload("res://resources/items/sugar_slot.png"),
+	"vegetables": preload("res://resources/items/veg_slot.png")
 }
 
 func _ready() -> void:
